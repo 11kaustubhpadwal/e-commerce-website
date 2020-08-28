dae5461f00cad2528cbf35815e36a9930c8f98e3
Kaustubh Padwal <11kaustubhpadwal@gmail.com>
2020-08-28 16:17:10 +0200

    Invalid auth warning added.

--- pages/my-account.js
+++ pages/my-account.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Message } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
+import { Divider } from "semantic-ui-react";
 
-const MyAccount = ({ history }) => {
+const MyAccount = () => {
   const { isAuthenticated } = useAuth0();
 
   if (isAuthenticated) {
     return <div>My account</div>;
   } else {
-    return <div>Please login.</div>;
+    return (
+      <Grid container padded doubling stackable>
+        <Message
+          warning
+          style={{ width: "100%" }}
+          icon="warning sign"
+          header="Please login!"
+          content="Click the login button in the top right corner and try again."
+        ></Message>
+      </Grid>
+    );
   }
 };
 
